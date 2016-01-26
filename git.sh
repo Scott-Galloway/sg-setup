ccea63ec92683e36fdc02679507f5ead20516cb4
Scott Galloway <gallowsa@gmail.com>
2016-01-26 09:59:09 -0500

    modified:   git.sh

--- git.sh
+++ git.sh
@@ -14,8 +14,8 @@ __BASE="$(basename ${__FILE} .sh)"
 ##Setup Git variables
 
 git config --global user.name "Scott Galloway"
-git congig --global user.email "gallowsa@gmail.com"
+git config --global user.email "gallowsa@gmail.com"
 git config --global credential.helper cache
-git congig --global credential.helper 'cache timeout=3600'
+git config --global credential.helper 'cache timeout=3600'
 
 exit 0
